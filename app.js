2cf1b9369d4f97c918163993f99966b0b3da68e2
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-20 11:55:35 +0200

    type

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const { add } = require('./math')
+import { add } from "./math.js";
+import fs from "fs/promises";
 
-add(2,2)
+add(2, 2);
+
+await fs.writeFile("text.txt", "Fs worked");
+const data = await fs.readFile("text.txt", "utf-8");
+console.log(data);
+
+import os from "os";
+
+console.log("Платформа:", os.platform());
+console.log("Архитектура:", os.arch());
+console.log("Кол-во CPU:", os.cpus().length);
+console.log("Свободная память (MB):", os.freemem() / 1024 / 1024);
