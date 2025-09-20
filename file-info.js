8c6e17fdde396d9db14b2bec7c0fcf4966113a7c
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-20 13:18:55 +0200

    file info

---
+++ file-info.js
@@ -0,0 +1,19 @@
+import path from "path";
+import fs from "fs/promises"
+import { log } from "console";
+import { fileURLToPath } from "url";
+const text = './text.txt'
+
+export const info = async() => {
+        const data = (await fs.readFile('text.txt', "utf-8")).slice(0, 100)
+    console.log("Text 100:", data);
+
+    const showPath = path.resolve(text);
+    log("PATH", showPath);
+    const file = await fs.stat(text);
+    console.log("Size", file.size);
+    const txt = path.extname(text);
+    console.log("TXT", txt);
+    
+    
+}
