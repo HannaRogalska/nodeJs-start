89fe6db313c925ace512ea41b301a5b10e0dfd32
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-24 17:50:52 +0200

    date

--- module.js
+++ module.js
@@ -1,23 +1,21 @@
 import { log } from "console";
 import fs from "fs/promises";
-import os from "os"
+import os from "os";
 
 const text = "./text.txt";
 
-
 export const moduleFs = async () => {
-    // const dataText = await fs.readFile(text, 'utf-8');
-    // await fs.writeFile("./copy/copy.txt", dataText.slice(0, 10));
-    const nameHost = os.hostname().toUpperCase().slice(0, 17)
-    const arc = os.arch()
-    const plat = os.platform()
-    const data = new Date()
-    await fs.writeFile(
-      "./copy/copy.txt",
-      `${nameHost} ${"\n"} ${arc} - ${plat} ${"\n"} ${
-        os.cpus().length
-      } ${"\n"} Data ${data} `
-    );
-    
-};
+  // const dataText = await fs.readFile(text, 'utf-8');
+  // await fs.writeFile("./copy/copy.txt", dataText.slice(0, 10));
+  const nameHost = os.hostname().toUpperCase().slice(0, 17);
+  const arc = os.arch();
+  const plat = os.platform();
+  const data = new Date();
 
+  await fs.writeFile(
+    "./copy/copy.txt",
+    `${nameHost} ${"\n"} ${arc} - ${plat} ${"\n"} ${
+      os.cpus().length
+    } ${"\n"} Day: ${data.getDay()} ${"\n"} Month: ${data.getMonth()}${"\n"} Year: ${data.getFullYear()} ${"\n"} Time: ${data.getHours()}:${data.getMinutes()}`
+  );
+};
