c3c34891ddf095edfd48eae6cc2fb43166b797dc
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-25 13:04:17 +0200

    emit

--- process.js
+++ process.js
@@ -1,17 +1,22 @@
+import EventEmitter from "events";
 
+const emitter = new EventEmitter();
 export const processArv = () => {
-    const arg = process.argv[2]
+const arg = process.argv[2];
+    emitter.on("check",(value) => {
+        if (value !== "Hanna") {
+          console.log(value);
+        } else {
+          console.log("Hello", value);
+        }
+    })
+    
     if (!arg) {
       console.log("Error");
       process.exit(1);
     }
-    if (arg !== "Hanna") {
-        console.log(arg)
-    } else {
-        console.log("Hello", arg);
-    }
-
     
+    emitter.emit("check", arg);
 }
 
 
