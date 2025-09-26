0271e42b13db42c4ccc1cefcdf55e305e3ac4573
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-26 19:27:05 +0200

    read and write

--- stream_file.js
+++ stream_file.js
@@ -5,17 +5,18 @@ const readFileStr = fs.createReadStream("./todo.txt", "utf-8");
 const writeFileStr = fs.createWriteStream("./copy_todo.txt");
 export const chunk = () => {
   readFileStr.on("data", (todos) => {
-    log(todos.length);
+     log(todos)
   });
   readFileStr.on("end", () => {
-    log("✅ Finished reading file");
-  });
-  readFileStr.on("error", (err) => {
-    log("❌ Error:", err.message);
-  });
     writeFileStr.write("Add new todo");
     writeFileStr.end();
     writeFileStr.on("finish", () => {
       console.log("New todo created ✅");
     });
+  });
+  readFileStr.on("error", (err) => {
+    log("❌ Error:", err.message);
+  });
+   readFileStr.pipe(writeFileStr, {end: false});
+    
 };
