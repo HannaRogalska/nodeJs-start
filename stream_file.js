856db362135bd18b33b4364031d8ba9565c43fb5
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-26 18:30:56 +0200

    chunk write

--- stream_file.js
+++ stream_file.js
@@ -1,16 +1,21 @@
-import { log } from "console"
-import fs from "fs"
+import { log } from "console";
+import fs from "fs";
 
-
- const readFileStr = fs.createReadStream("./todo.txt", "utf-8");
+const readFileStr = fs.createReadStream("./todo.txt", "utf-8");
+const writeFileStr = fs.createWriteStream("./copy_todo.txt");
 export const chunk = () => {
-    readFileStr.on("data", (todos) => {
-        log(todos.length)
-    })
-     readFileStr.on("end", () => {
-       log("✅ Finished reading file");
-     });
-    readFileStr.on("error", (err) => {
-      log("❌ Error:", err.message);
+  readFileStr.on("data", (todos) => {
+    log(todos.length);
+  });
+  readFileStr.on("end", () => {
+    log("✅ Finished reading file");
+  });
+  readFileStr.on("error", (err) => {
+    log("❌ Error:", err.message);
+  });
+    writeFileStr.write("Add new todo");
+    writeFileStr.end();
+    writeFileStr.on("finish", () => {
+      console.log("New todo created ✅");
     });
-}
+};
