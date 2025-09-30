c54767b2c86913581828a79617994c038539b535
Hanna Rogalska <hanrogalska@gmail.com>
2025-09-30 15:37:02 +0200

    finish server

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-import { log } from "console";
 import express from "express";
 import fs from "fs";
+import fsp from "fs/promises";
 
 const app = express();
 const PORT = 3000;
@@ -8,10 +8,31 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Todo API 🚀" });
+  res.status(200).json({ message: "Welcome to Todo API 🚀" });
 });
-app.get("todos", (req, res) => {
-  const todoWithStream = fs.createReadStream("./todo.txt");
-  todoWithStream.pipe(res);
+app.get("/todos", (req, res) => {
+    let allTodosFromTxt = ""
+    
+    const todoWithStream = fs.createReadStream("./todo.txt");
+    todoWithStream.on("data", (chunk) => {
+        allTodosFromTxt += chunk
+    })
+    todoWithStream.on("end", () => {
+        const todosIn = allTodosFromTxt.split('\n')
+        res.status(200).json({ "data": todosIn})
+    })
+  todoWithStream.on("error", (err) => {
+    res.status(500).json({ message: "No todos yet!" });
+    console.log(err);
+  });
 });
 
+app.post("/add", async (req, res) => {
+    const { todo } = req.body;
+    if(!todo) res.status(400).json({ message: "Todo is required" });
+  await fsp.appendFile("./todo.txt", todo);
+  res.status(201).json({ message: "Todo added", todo });
+});
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+});
